c1ecbab59fd5696f2c12a93a364b3657aaee5174
Carsten Meyke <carsten.meyke@gmail.com>
2021-06-13 14:01:05 +0200

    openzeppelin enabled

---
+++ contracts/Color.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/security/Pausable.sol';
+import '@openzeppelin/contracts/access/AccessControl.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+
+contract Color is
+  ERC721,
+  ERC721Enumerable,
+  ERC721URIStorage,
+  Pausable,
+  AccessControl,
+  ERC721Burnable
+{
+  using Counters for Counters.Counter;
+
+  bytes32 public constant PAUSER_ROLE = keccak256('PAUSER_ROLE');
+  bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+  Counters.Counter private _tokenIdCounter;
+
+  mapping(string => bool) private _colorExists;
+
+  constructor() ERC721('Color', 'COLOR') {
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _setupRole(PAUSER_ROLE, msg.sender);
+    _setupRole(MINTER_ROLE, msg.sender);
+  }
+
+  function mint(string memory _color) public {
+    require(hasRole(MINTER_ROLE, msg.sender));
+    require(!_colorExists[_color], 'Color already exists.');
+    _safeMint(msg.sender, _tokenIdCounter.current());
+    _setTokenURI(_tokenIdCounter.current(), _color);
+    _tokenIdCounter.increment();
+    _colorExists[_color] = true;
+  }
+
+  function pause() public {
+    require(hasRole(PAUSER_ROLE, msg.sender));
+    _pause();
+  }
+
+  function unpause() public {
+    require(hasRole(PAUSER_ROLE, msg.sender));
+    _unpause();
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 tokenId
+  ) internal override(ERC721, ERC721Enumerable) whenNotPaused {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+  function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    super._burn(tokenId);
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+  {
+    return super.tokenURI(tokenId);
+  }
+
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, ERC721Enumerable, AccessControl)
+    returns (bool)
+  {
+    return super.supportsInterface(interfaceId);
+  }
+}
